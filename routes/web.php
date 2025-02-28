b97f8e096b9a2318ae44c7632ca3219071b17ceb
TNTTheLagger <tntcsatornaja@gmail.com>
2025-02-28 10:26:17 +0100

    Revert "Added vue files"
    This reverts commit 926e9f995c51a278daf136d1e5566def3e5515f2.

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/barbers', function () {
-    return Inertia::render('Barbers');
-});
-
-Route::get('/appointments', function () {
-    return Inertia::render('Appointments');
 });
