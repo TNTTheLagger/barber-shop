926e9f995c51a278daf136d1e5566def3e5515f2
TNTTheLagger <tntcsatornaja@gmail.com>
2025-02-28 09:53:04 +0100

    Added vue files

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/barbers">Barbers</a></li>
+                <li><a href="/appointments">Appointments</a></li>
+            </ul>
+        </nav>
+        <main>
+            <slot></slot>
+        </main>
+    </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/barbers', function () {
+    return Inertia::render('Barbers');
+});
+
+Route::get('/appointments', function () {
+    return Inertia::render('Appointments');
 });
